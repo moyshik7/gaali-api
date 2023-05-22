437b39104e69e896ba161f867d7b930d94153701
moyshik7 <moyshik7@gmail.com>
2023-05-22 15:02:22 +0600

    Now it works fine

---
+++ createdb.js
@@ -0,0 +1,23 @@
+
+
+import Database from "better-sqlite3";
+import fs from "fs"
+
+/*const insert = db.prepare(`CREATE TABLE IF NOT EXISTS bangla (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+   	text TEXT NOT NULL
+);`);*/
+
+const insert = db.prepare(`INSERT INTO bangla (text) VALUES (@gali)`)
+//const insert = db.prepare(`SELECT text FROM bangla ORDER BY RANDOM() LIMIT 1;`)
+/*
+fs.readFileSync('pp.txt', 'UTF-8').split(/\r?\n/).forEach(function(line){
+    if(line.length >= 1){
+        insert.run({gali: line})
+        console.log("Inserted: gali")
+    }
+    console.log("blank line")
+})
+*/
+
+//console.log(insert.get())
--- main.js
+++ main.js
@@ -1,28 +1,25 @@
 import express from "express";
 import Database from "better-sqlite3";
 
-const db = new Database("./db/gaali.db", { verbose: console.log });
+const db = new Database("./db/gaali.db");
 db.pragma('journal_mode = WAL');
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send("hello world")
+})
+
+app.get("/bn/", (req, res) => {
+    const q = db.prepare(`SELECT text FROM bangla ORDER BY RANDOM() LIMIT 1;`).get();
+    const r = {
+        code: 200,
+        gali: q.text,
+        hash: parseInt(Math.floor(Date.now() / 1000).toString() + Math.floor(Math.random() * 100000).toString())
+    }
+    res.send(r)
 })
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening on port ${process.env.PORT || 3000}`)
 })
-
-
-/*const insert = db.prepare(`CREATE TABLE IF NOT EXISTS bangla (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-   	text TEXT NOT NULL
-);`);*/
-
-//const insert = db.prepare(`INSERT INTO bangla (text) VALUES (@gali)`)
-const insert = db.prepare(`SELECT text FROM bangla ORDER BY RANDOM() LIMIT 1;`)
-
-//insert.run({gali: "খানকির পোলা"})
-
-console.log(insert.get())

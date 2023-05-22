bc6ed8cdf8e32ce36206ee79bdac403ba7075b63
moyshik7 <moyshik7@gmail.com>
2023-05-22 17:54:38 +0600

    Added english galis to db

--- createdb.js
+++ createdb.js
@@ -8,21 +8,24 @@ db.pragma('journal_mode = WAL');
 
 
 
-/*const insert = db.prepare(`CREATE TABLE IF NOT EXISTS bangla (
+/*const insert = db.prepare(`CREATE TABLE IF NOT EXISTS english (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
    	text TEXT NOT NULL
 );`);*/
 
-const insert = db.prepare(`INSERT INTO bangla (text) VALUES (@gali)`)
+const insert = db.prepare(`INSERT INTO english (text) VALUES (@gali)`)
 //const insert = db.prepare(`SELECT text FROM bangla ORDER BY RANDOM() LIMIT 1;`)
 
-fs.readFileSync('pp.txt', 'UTF-8').split(/\r?\n/).forEach(function(line){
+fs.readFileSync('gali-en.txt', 'UTF-8').split(/\r?\n/).forEach(function(line){
     if(line.length >= 1){
         insert.run({gali: line})
         console.log(`Inserted: ${line}`)
+    } else {
+        console.log("blank line")
     }
-    console.log("blank line")
 })
 
 
 //console.log(insert.get())
+
+//insert.run()

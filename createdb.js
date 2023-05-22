cc64f3350e3a9f83b8d71ab814f5fee1d96a6314
moyshik7 <moyshik7@gmail.com>
2023-05-22 18:11:26 +0600

    Adding ip logger to fight against spoofers

--- createdb.js
+++ createdb.js
@@ -5,27 +5,27 @@ import fs from "fs"
 
 const db = new Database("./db/gaali.db");
 db.pragma('journal_mode = WAL');
+const ip = new Database("./db/ip.db");
+db.pragma('journal_mode = WAL');
 
+const insert = ip.prepare(`CREATE TABLE ip (
+	time INTEGER,
+   	ip TEXT NOT NULL
+);`);
 
-
-/*const insert = db.prepare(`CREATE TABLE IF NOT EXISTS english (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-   	text TEXT NOT NULL
-);`);*/
-
-const insert = db.prepare(`INSERT INTO english (text) VALUES (@gali)`)
+//const insert = db.prepare(`INSERT INTO english (text) VALUES (@gali)`)
 //const insert = db.prepare(`SELECT text FROM bangla ORDER BY RANDOM() LIMIT 1;`)
 
-fs.readFileSync('gali-en.txt', 'UTF-8').split(/\r?\n/).forEach(function(line){
+/*fs.readFileSync('gali-en.txt', 'UTF-8').split(/\r?\n/).forEach(function(line){
     if(line.length >= 1){
         insert.run({gali: line})
         console.log(`Inserted: ${line}`)
     } else {
         console.log("blank line")
     }
-})
+})*/
 
 
 //console.log(insert.get())
 
-//insert.run()
+insert.run()
--- main.js
+++ main.js
@@ -3,11 +3,14 @@ import Database from "better-sqlite3";
 
 const db = new Database("./db/gaali.db");
 db.pragma('journal_mode = WAL');
+const ip = new Database("./db/ip.db");
+ip.pragma('journal_mode = WAL');
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.send("hello world")
+    SaveIP(req)
 })
 
 app.get("/bn/", (req, res) => {
@@ -18,6 +21,7 @@ app.get("/bn/", (req, res) => {
         gali: q.text
     }
     res.send(r)
+    SaveIP(req)
 })
 app.get("/en/", (req, res) => {
     const q = db.prepare(`SELECT text FROM english ORDER BY RANDOM() LIMIT 1;`).get();
@@ -27,8 +31,18 @@ app.get("/en/", (req, res) => {
         gali: q.text
     }
     res.send(r)
+    SaveIP(req)
 })
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening on port ${process.env.PORT || 3000}`)
 })
+
+const SaveIP = (req) => {
+    const q = ip.prepare(`INSERT INTO ip (time, ip) VALUES (@time, @ip)`)
+    q.run({
+        ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress ,
+        time: Date.now()
+    })
+    console.log(ip.prepare(`SELECT * FROM IP`).get())
+}

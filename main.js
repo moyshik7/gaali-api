5339f6010a4e8c4cebe8c5a183c61c37afd3a67c
moyshik7 <moyshik7@gmail.com>
2023-05-22 14:31:46 +0600

    Done playing with sqlite

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-const express = require('express')
+import express from "express";
+import Database from "better-sqlite3";
+
+const db = new Database("./db/gaali.db", { verbose: console.log });
+db.pragma('journal_mode = WAL');
+
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Example app listening on port ${process.env.PORT || 3000}`)
 })
+
+
+/*const insert = db.prepare(`CREATE TABLE IF NOT EXISTS bangla (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+   	text TEXT NOT NULL
+);`);*/
+
+//const insert = db.prepare(`INSERT INTO bangla (text) VALUES (@gali)`)
+const insert = db.prepare(`SELECT text FROM bangla ORDER BY RANDOM() LIMIT 1;`)
+
+//insert.run({gali: "খানকির পোলা"})
+
+console.log(insert.get())
